6347f42fcf14f2645396ce6bf6e1ede4c7ccb142
Eduardo Hidalgo <hidalgojose86@gmail.com>
2024-11-23 21:05:53 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,8 +17,6 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // Conexión a MongoDB
 mongoose
-  console.log("procesooooo......" , process.env.MONGO_URI)
-  console.log("procesooooo......" , process.env.MONGO_URL)
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Conectado a MongoDB"))
   .catch((err) => console.error("Error al conectar a MongoDB", err));
